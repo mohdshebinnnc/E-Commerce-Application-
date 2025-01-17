e4413f056287ac84247ace1f1bd2729b3949f116
shebin <mohammedshebinc92@gmail.com>
2025-01-17 17:17:13 +0530

    New files update

---
+++ Backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose= require('mongoose')
+
+const app=express()
+const PORT=8084
+
+let connection= mongoose.connect("mongodb+srv://mohammedshebinc92:5zU3VGl55Wr9xw9r@cluster0.enuvl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+app.get("/ping",(req,res) => {
+    res.send("pong")
+})
+
+app.get("/",(req,res) => {
+    res.send("ching")
+})
+
+app.listen(PORT,async()=>{
+    try{
+        await connection;
+        console.log("Successfully connected to MongoDB");
+    } catch (error){
+        console.log(error);
+    }
+    
+        console.log(`Server is running on port ${PORT}`)
+    })
